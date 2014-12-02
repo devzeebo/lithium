d47c878df2fdc599855249bf76dea9b1bedf1d9b
Eric Siebeneich <eric.siebeneich@exclusr.com>
2014-12-01 20:52:37 -0600

    Fixed never handling bounced messages

--- src/main/com/zeebo/lithium/mesh/MeshNode.groovy
+++ src/main/com/zeebo/lithium/mesh/MeshNode.groovy
@@ -20,71 +20,71 @@ import java.util.logging.Level
 @Log
 class MeshNode {
 
-	static final Gson gson = new Gson()
-	static String delimiter = '\0'
+    static final Gson gson = new Gson()
+    static String delimiter = '\0'
 
-	ServerSocket serverSocket
-	String serverId
+    ServerSocket serverSocket
+    String serverId
 
-	def sockets = [:] as ConcurrentHashMap
-	def messages = new MessageBuffer()
+    def sockets = [:] as ConcurrentHashMap
+    def messages = new MessageBuffer()
 
-	private List<MessageHandler> messageHandlers = []
+    private List<MessageHandler> messageHandlers = []
 
-	private Message heartbeat
+    private Message heartbeat
 
-	MeshNode() {
-		this(40026)
-	}
+    MeshNode() {
+        this(40026)
+    }
 
-	MeshNode(int port) {
-		this(InetAddress.localHost.hostAddress + ':' + port, port)
-	}
+    MeshNode(int port) {
+        this(InetAddress.localHost.hostAddress + ':' + port, port)
+    }
 
-	MeshNode(String serverName, int port) {
+    MeshNode(String serverName, int port) {
 
-		serverId = serverName
-		serverSocket = new ServerSocket(port)
+        serverId = serverName
+        serverSocket = new ServerSocket(port)
 
-		addMessageHandler(new SystemMessageHandler())
+        addMessageHandler(new SystemMessageHandler())
 
-		log.info "$serverId started on port $port"
+        log.info "$serverId started on port $port"
 
-		Thread.startDaemon {
+        Thread.startDaemon {
 
-			heartbeat = new Message(messageType: SystemMessageHandler.TYPE_HEARTBEAT)
+            heartbeat = new Message(messageType: SystemMessageHandler.TYPE_HEARTBEAT)
 
-			while(true) {
+            while (true) {
 
-				def time = System.currentTimeMillis()
-				sockets.findAll { k, v -> v.timeout + 5000 < time }.each { k, v ->
-					log.info "Timeout detected: Closing connection to $k"
-					sockets.remove(k)
-				}
+                def time = System.currentTimeMillis()
+                sockets.findAll { k, v -> v.timeout + 5000 < time }.each { k, v ->
+                    log.info "Timeout detected: Closing connection to $k"
+                    sockets.remove(k)
+                }
 
-				sleep 1000
-				sendAll(heartbeat)
-			}
-		}
-	}
+                sleep 1000
+                sendAll(heartbeat)
+            }
+        }
+    }
 
-	def addMessageHandler(MessageHandler handler) {
-		def collision = null
-		if (!(collision = messageHandlers.find { it.typeRange.intersect(handler.typeRange) })) {
-			handler.node = this
-			messageHandlers << handler
-		} else {
-			log.severe "Cannot add handler ${handler.class}. Collision on type range with ${collision.class}: ${collision.typeRange.from}-${collision.typeRange.to}"
-		}
-	}
+    def addMessageHandler(MessageHandler handler) {
+        def collision = null
+        if (!(collision = messageHandlers.find { it.typeRange.intersect(handler.typeRange) })) {
+            handler.node = this
+            messageHandlers << handler
+        } else {
+            log.severe "Cannot add handler ${handler.class}. Collision on type range with ${collision.class}: ${collision.typeRange.from}-${collision.typeRange.to}"
+        }
+    }
 
-	def listen() {
-		Thread.start {
-			while (true) {
-				serverSocket.accept(true, handleSocket)
-			}
-		}
-	}
+    def listen() {
+        Thread.start {
+            while (true) {
+                serverSocket.accept(true, handleSocket)
+            }
+        }
+    }
 
     def connect(String hostname, int port, Closure callback = null) {
         log.info "$serverId: attempting to connect to $hostname:$port"
@@ -97,141 +97,140 @@ class MeshNode {
         }
     }
 
-	private def sendHelper(PrintWriter output, Message message) {
+    private def sendHelper(PrintWriter output, Message message) {
 
-		if (!message.sender) {
-			message.sender = serverId
-		}
+        if (!message.sender) {
+            message.sender = serverId
+        }
 
-		if (message.messageType > 1) {
-			messages.setMessage(message)
-		}
+        if (message.messageType > 1) {
+            messages.setMessage(message)
+        }
 
-		output.print(message.toString() + delimiter)
-		output.flush()
-	}
+        output.print(message.toString() + delimiter)
+        output.flush()
+    }
 
-	def send(String remoteId, Message message) {
-		if (sockets.containsKey(remoteId)) {
-			try {
-				sendHelper(sockets[remoteId].output as PrintWriter, message)
-			}
-			catch(SocketException e) {
-				e.printStackTrace()
-				sockets.remove(remoteId)
-			}
-		}
-	}
+    def send(String remoteId, Message message) {
+        if (sockets.containsKey(remoteId)) {
+            try {
+                sendHelper(sockets[remoteId].output as PrintWriter, message)
+            }
+            catch (SocketException e) {
+                e.printStackTrace()
+                sockets.remove(remoteId)
+            }
+        }
+    }
 
-	def sendAll(String[] remoteIds, Message message) {
-		remoteIds.each {
-			send(it, message)
-		}
-	}
+    def sendAll(String[] remoteIds, Message message) {
+        remoteIds.each {
+            send(it, message)
+        }
+    }
 
-	def sendAll(Message message) {
-		sendAll(sockets.keySet() as String[], message)
-	}
+    def sendAll(Message message) {
+        sendAll(sockets.keySet() as String[], message)
+    }
 
-	private def handleSocket = { Socket socket, Closure callback ->
+    private def handleSocket = { Socket socket, Closure callback ->
 
-		log.info "$serverId: connected to ${socket.remoteSocketAddress}"
+        log.info "$serverId: connected to ${socket.remoteSocketAddress}"
 
-		BufferedReader input = socket.inputStream.newReader()
-		PrintWriter output = socket.outputStream.newPrintWriter()
+        BufferedReader input = socket.inputStream.newReader()
+        PrintWriter output = socket.outputStream.newPrintWriter()
 
-		Message serverInfo = new Message()
+        Message serverInfo = new Message()
 
-		log.finest "$serverId: sending server info to ${socket.remoteSocketAddress}"
-		sendHelper(output, serverInfo)
+        log.finest "$serverId: sending server info to ${socket.remoteSocketAddress}"
+        sendHelper(output, serverInfo)
 
-		use(ReaderCategory) {
+        use(ReaderCategory) {
 
-			log.finest "$serverId: awaiting response from ${socket.remoteSocketAddress}"
+            log.finest "$serverId: awaiting response from ${socket.remoteSocketAddress}"
 
-			String messageString = input.readUntil(delimiter)
-			Message message = Message.fromJson(messageString)
-			String remoteServerId = message.sender
+            String messageString = input.readUntil(delimiter)
+            Message message = Message.fromJson(messageString)
+            String remoteServerId = message.sender
 
-			log.info "$serverId: received server info from ${socket.remoteSocketAddress}. Identifying as $remoteServerId"
+            log.info "$serverId: received server info from ${socket.remoteSocketAddress}. Identifying as $remoteServerId"
 
-			if (!sockets[remoteServerId]) {
+            if (!sockets[remoteServerId]) {
 
-				log.info "$serverId: connection confirmed to $remoteServerId"
-				sockets[remoteServerId] = [socket: socket, input: input, output: output, timeout: System.currentTimeMillis()]
+                log.info "$serverId: connection confirmed to $remoteServerId"
+                sockets[remoteServerId] = [socket: socket, input: input, output: output, timeout: System.currentTimeMillis()]
 
                 callback sockets.find { k, v -> k == remoteServerId }
 
-				while (true) {
-					messageString = input.readUntil(delimiter)
-					if (messageString) {
-						handleMessage remoteServerId, Message.fromJson(messageString)
-					}
-				}
-			} else {
-				log.fine "${serverId}: Connection to $remoteServerId already exists. Closing ${socket.localSocketAddress}"
-				input.close()
-				output.close()
-				socket.close()
+                while (true) {
+                    messageString = input.readUntil(delimiter)
+                    if (messageString) {
+                        handleMessage remoteServerId, Message.fromJson(messageString)
+                    }
+                }
+            } else {
+                log.fine "${serverId}: Connection to $remoteServerId already exists. Closing ${socket.localSocketAddress}"
+                input.close()
+                output.close()
+                socket.close()
 
                 callback null
-			}
-		}
-	}
+            }
+        }
+    }
 
-	private def handleMessage(String remoteServerId, Message message) {
+    private def handleMessage(String remoteServerId, Message message) {
 
-		assert message != null
-		log.log(message.messageType == 1 ? Level.FINEST : Level.FINE,
-				"$serverId: received message from $remoteServerId> ${message.sender} : ${message.messageType}")
-		if (sockets[message.sender]) {
-			sockets[message.sender]?.timeout = System.currentTimeMillis()
+        assert message != null
+        log.log(message.messageType == 1 ? Level.FINEST : Level.FINE,
+                "$serverId: received message from $remoteServerId> ${message.sender} : ${message.messageType}")
 
-			MessageHandler handler = messageHandlers.find { it.typeRange.contains(message.messageType) }
-			handler?.handleMessage(message)
+        sockets[message.sender]?.timeout = System.currentTimeMillis()
 
-			messages.setMessage(message)
+        MessageHandler handler = messageHandlers.find { it.typeRange.contains(message.messageType) }
+        handler?.handleMessage(message)
 
-			if (handler?.class != SystemMessageHandler) {
-				log.fine "$serverId: rebroadcasting message to ${sockets.keySet().findAll { it != remoteServerId }}"
-				Message msg = new Message()
-				msg.messageId = message.messageId
+        messages.setMessage(message)
 
-				sendAll(sockets.keySet().findAll { it != remoteServerId } as String[], msg)
-			}
-		}
-	}
+        if (handler?.class != SystemMessageHandler) {
+            log.fine "$serverId: rebroadcasting message to ${sockets.keySet().findAll { it != remoteServerId }}"
+            Message msg = new Message()
+            msg.messageId = message.messageId
 
-	public static void main(String[] args) {
+            sendAll(sockets.keySet().findAll { it != remoteServerId } as String[], msg)
+        }
+    }
 
-		def argMap = [serverName: UUID.randomUUID().toString(), port: 40026]
-		args.each {
-			it.split('=').with {
-				argMap[it[0]] = it[1]
-			}
-		}
+    public static void main(String[] args) {
 
-		def neg1 = new MeshNode(argMap.serverName, argMap.port as int)
-		neg1.addMessageHandler(new ChatMessageHandler())
-		neg1.listen()
+        def argMap = [serverName: UUID.randomUUID().toString(), port: 40026]
+        args.each {
+            it.split('=').with {
+                argMap[it[0]] = it[1]
+            }
+        }
 
-		Scanner scan = new Scanner(System.in)
+        def neg1 = new MeshNode(argMap.serverName, argMap.port as int)
+        neg1.addMessageHandler(new ChatMessageHandler())
+        neg1.listen()
 
-		while(true) {
-			String s = scan.nextLine()
+        Scanner scan = new Scanner(System.in)
 
-			switch( s.substring(0, (s.indexOf(' ') + 1 ?: s.length()) - 1) ) {
-				case 'q':
-					System.exit(0)
-				case 'connect':
-					def host = s.substring(s.indexOf(' ') + 1).split(':')
-					neg1.connect(host[0], host[1] as int) { println "Connected to $it!" }
-					break;
-				default:
-					Message msg = new Message(messageType: ChatMessageHandler.TYPE_CHAT_MESSAGE)
-					msg.data.contents = s
-					neg1.sendAll(neg1.sockets.keySet() as String[], msg)
-			}
-		}
-	}
+        while (true) {
+            String s = scan.nextLine()
+
+            switch (s.substring(0, (s.indexOf(' ') + 1 ?: s.length()) - 1)) {
+                case 'q':
+                    System.exit(0)
+                case 'connect':
+                    def host = s.substring(s.indexOf(' ') + 1).split(':')
+                    neg1.connect(host[0], host[1] as int) { println "Connected to $it!" }
+                    break;
+                default:
+                    Message msg = new Message(messageType: ChatMessageHandler.TYPE_CHAT_MESSAGE)
+                    msg.data.contents = s
+                    neg1.sendAll(neg1.sockets.keySet() as String[], msg)
+            }
+        }
+    }
 }
