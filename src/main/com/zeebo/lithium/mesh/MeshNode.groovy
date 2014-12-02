49f38575ab1366765c833b65fc367218e0fe0bdd
Eric Siebeneich <eric.siebeneich@exclusr.com>
2014-12-01 19:47:45 -0600

    Added callback to connect method

--- src/main/com/zeebo/lithium/mesh/MeshNode.groovy
+++ src/main/com/zeebo/lithium/mesh/MeshNode.groovy
@@ -86,17 +86,16 @@ class MeshNode {
 		}
 	}
 
-	def connect(String hostname, int port) {
+    def connect(String hostname, int port, Closure callback = null) {
+        log.info "$serverId: attempting to connect to $hostname:$port"
 
-		log.info "$serverId: attempting to connect to $hostname:$port"
+        Socket socket = new Socket(hostname, port)
+        log.fine "$serverId: connection established ${socket.localSocketAddress} to $hostname:$port"
 
-		Socket socket = new Socket(hostname, port)
-		log.fine "$serverId: connection established ${socket.localSocketAddress} to $hostname:$port"
-
-		Thread.start {
-			handleSocket socket
-		}
-	}
+        Thread.start {
+            handleSocket socket, callback
+        }
+    }
 
 	private def sendHelper(PrintWriter output, Message message) {
 
@@ -134,7 +133,7 @@ class MeshNode {
 		sendAll(sockets.keySet() as String[], message)
 	}
 
-	private def handleSocket = { Socket socket ->
+	private def handleSocket = { Socket socket, Closure callback ->
 
 		log.info "$serverId: connected to ${socket.remoteSocketAddress}"
 
@@ -161,6 +160,8 @@ class MeshNode {
 				log.info "$serverId: connection confirmed to $remoteServerId"
 				sockets[remoteServerId] = [socket: socket, input: input, output: output, timeout: System.currentTimeMillis()]
 
+                callback sockets.find { k, v -> k == remoteServerId }
+
 				while (true) {
 					messageString = input.readUntil(delimiter)
 					if (messageString) {
@@ -172,6 +173,8 @@ class MeshNode {
 				input.close()
 				output.close()
 				socket.close()
+
+                callback null
 			}
 		}
 	}
@@ -222,7 +225,7 @@ class MeshNode {
 					System.exit(0)
 				case 'connect':
 					def host = s.substring(s.indexOf(' ') + 1).split(':')
-					neg1.connect(host[0], host[1] as int)
+					neg1.connect(host[0], host[1] as int) { println "Connected to $it!" }
 					break;
 				default:
 					Message msg = new Message(messageType: ChatMessageHandler.TYPE_CHAT_MESSAGE)
